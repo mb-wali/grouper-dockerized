3f99ba15c46c87b2a572c7f62b10e2ed2d03a7d2
John Gasper <jtgasper3@gmail.com>
2016-04-20 14:12:13 -0700

    re-tagging

--- manualBuild.sh
+++ manualBuild.sh
@@ -1,4 +1,5 @@
-docker build --tag=unicon/grouper-java-base:7u95 java-base \
+docker pull centos:centos7 \
+&& docker build --tag=unicon/grouper-java-base:7u95 java-base \
 && docker build --tag=unicon/grouper-api:2.2.2 api \
 && docker build --tag=unicon/grouper-daemon:2.2.2 daemon \
 && docker build --tag=unicon/grouper-web-base:2.2.2 web-base \
